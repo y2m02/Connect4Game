f24fd9ce3766d404acd4a172ff7742237dd5ba24
raimyy2m <raimymejia@gmail.com>
2021-12-04 20:25:31 -0400

    Unit tests part 2

--- Connect4Tests/Connect4Tests.cs
+++ Connect4Tests/Connect4Tests.cs
@@ -18,7 +18,6 @@ namespace Connect4Tests
         {
             var result = game.Play(0);
 
-
             Assert.That(result, Is.EqualTo("It's player 2 turn"));
         }
 
@@ -43,6 +42,148 @@ namespace Connect4Tests
             var result = game.Play(0);
 
             Assert.That(result, Is.EqualTo("Column full"));
+        }
+
+        [Test]
+        public void Play_PlayerOneWonVertically_ReturnPlayerOneWon()
+        {
+            game.Play(0); // 1
+            game.Play(1); // 2
+            game.Play(0); // 1
+            game.Play(1); // 2
+            game.Play(0); // 1
+            game.Play(1); // 2
+            var result = game.Play(0); // 1
+
+            Assert.That(result, Is.EqualTo("Player 1 won"));
+        }
+
+        [Test]
+        public void Play_PlayerTwoWonVertically_ReturnPlayerTwoWon()
+        {
+            game.Play(0); // 1
+            game.Play(1); // 2
+            game.Play(0); // 1
+            game.Play(1); // 2
+            game.Play(0); // 1
+            game.Play(1); // 2
+            game.Play(3); // 1
+            var result = game.Play(1); // 2
+
+            Assert.That(result, Is.EqualTo("Player 2 won"));
+        }
+
+        [Test]
+        public void Play_PlayerOneWonHorizontally_ReturnPlayerOneWon()
+        {
+            game.Play(0); // 1
+            game.Play(0); // 2
+            game.Play(1); // 1
+            game.Play(1); // 2
+            game.Play(2); // 1
+            game.Play(2); // 2
+            var result = game.Play(3); // 1
+
+            Assert.That(result, Is.EqualTo("Player 1 won"));
+        }
+
+        [Test]
+        public void Play_PlayerTwoWonHorizontally_ReturnPlayerTwoWon()
+        {
+            game.Play(0); // 1
+            game.Play(1); // 2
+            game.Play(1); // 1
+            game.Play(2); // 2
+            game.Play(2); // 1
+            game.Play(3); // 2
+            game.Play(3); // 1
+            var result = game.Play(4); // 2
+
+            Assert.That(result, Is.EqualTo("Player 2 won"));
+        }
+
+        [Test]
+        public void Play_PlayerOneWonAtSomeBottomDownwardDiagonalDiscs_ReturnPlayerOneWon()
+        {
+            game.Play(6); // 1
+            game.Play(0); // 2
+            game.Play(0); // 1
+            game.Play(0); // 2
+            game.Play(0); // 1
+            game.Play(1); // 2
+            game.Play(1); // 1
+            game.Play(2); // 2
+            game.Play(1); // 1
+            game.Play(4); // 2
+            game.Play(2); // 1
+            game.Play(4); // 2
+            var result = game.Play(3); // 1
+
+            Assert.That(result, Is.EqualTo("Player 1 won"));
+        }
+
+        [Test]
+        public void Play_PlayerTwoWonAtSomeBottomDownwardDiagonalDiscs_ReturnPlayerTwoWon()
+        {
+            game.Play(0); // 1
+            game.Play(0); // 2
+            game.Play(0); // 1
+            game.Play(0); // 2
+            game.Play(1); // 1
+            game.Play(1); // 2
+            game.Play(2); // 1
+            game.Play(1); // 2
+            game.Play(4); // 1
+            game.Play(2); // 2
+            game.Play(4); // 1
+            var result = game.Play(3); // 2
+
+            Assert.That(result, Is.EqualTo("Player 2 won"));
+        }
+
+        [Test]
+        public void Play_PlayerOneWonAtSomeTopDownwardDiagonalDiscs_ReturnPlayerOneWon()
+        {
+            game.Play(2); // 1
+            game.Play(2); // 2
+            game.Play(2); // 1
+            game.Play(2); // 2
+            game.Play(2); // 1
+            game.Play(3); // 2
+            game.Play(4); // 1
+            game.Play(5); // 2
+            game.Play(5); // 1
+            game.Play(4); // 2
+            game.Play(4); // 1
+            game.Play(3); // 2
+            game.Play(3); // 1
+            game.Play(0); // 2
+            var result = game.Play(3); // 1
+
+            Assert.That(result, Is.EqualTo("Player 1 won"));
+        }
+
+        [Test]
+        public void Play_PlayerTwoWonAtSomeTopDownwardDiagonalDiscs_ReturnPlayerTwoWon()
+        {
+            game.Play(6); // 1
+            game.Play(2); // 2
+            game.Play(2); // 1
+            game.Play(2); // 2
+            game.Play(2); // 1
+            game.Play(2); // 2
+            game.Play(3); // 1
+            game.Play(4); // 2
+            game.Play(5); // 1
+            game.Play(5); // 2
+            game.Play(4); // 1
+            game.Play(4); // 2
+            game.Play(3); // 1
+            game.Play(3); // 2
+            game.Play(0); // 1
+            var result = game.Play(3); // 2
+
+            Assert.That(result, Is.EqualTo("Player 2 won"));
         }
     }
 }
